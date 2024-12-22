ea970dc9d90bd8038405df7318a995787a8f4b03
verniy1606 <158278526+verniy1606@users.noreply.github.com>
2024-12-23 05:39:52 +0900

    SHHHHHHHHHHHHH FIX THE CONNECTION ERROR

--- commands/danbooru.py
+++ commands/danbooru.py
@@ -1,73 +1,83 @@
 import logging
 import asyncio
 
+from http.client import RemoteDisconnected
+from pybooru import PybooruHTTPError
+from requests.exceptions import ConnectionError
+
 import discord
 from discord.ext import commands
 
 from pybooru import Danbooru
+
 
 class DanbooruCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.danbo = Danbooru('danbooru')
 
+    async def get_posts(self, tag: str, tries=2) -> list:
+        for attempt in range(tries):
+            try:
+                posts = await asyncio.to_thread(
+                    self.danbo.post_list,
+                    limit=10,
+                    tags=tag,
+                    random=True
+                )
+                return posts
+            except ConnectionError as e:
+                if attempt < tries - 1:
+                    logging.error(
+                        f"Connection error: {e}. Trying again in 3 seconds.."
+                    )
+                    await asyncio.sleep(3)
+                else:
+                    logging.error('Could not reconnect to the remote.')
+                    return []
+            except PybooruHTTPError as e:
+                if '422' in e._msg:
+                    logging.error("Invalid tag !")
+                return []
+
     @commands.slash_command(name='danbooru', description='Pick random one image !')
-    async def danbo(
-        self, 
-        ctx: discord.ApplicationContext, 
-        index: discord.Option(
-            str,
-            description='When *searchtype* is set to *artist*, this property is used to search an user',
-            required=True
-        ), # type: ignore
-        searchtype: discord.Option(
-            str,
-            choices=[
-                'artist',
-                'global'
-            ],
-            description='just a test',
-            required=True,
-        ) # type: ignore
+    async def danbooru(
+        self,
+        ctx: discord.ApplicationContext,
+        tag: str
     ):
         await ctx.defer()
 
-        tag = ''
-
-        if searchtype == 'artist':
-            artists = await asyncio.to_thread(self.danbo.artist_list, index)
-            for artist in artists:
-                tag = artist['name']
-        elif searchtype == 'global':
-            tag = index
-
+        # Search the image
         logging.info(f'Searching for *{tag}*...')
-
-        posts = await asyncio.to_thread(self.danbo.post_list, limit=1, tags=tag, random=True)
+        posts = await self.get_posts(tag)
 
         if not posts:
             logging.info(f'Could not find the tag *{tag}* !')
-
             await ctx.respond(f'Could not find the tag *{tag}*...')
             return
 
+        # Find a correct image
+        sent = False
         for post in posts:
+            # Get variables
             file_url = post.get('file_url', 'Could not fetch the file...')
-            rating = post.get('rating', '?')
-            source = post.get('source', '?')
             id = post.get('id', '?')
-
             danbooru_url = f'https://danbooru.donmai.us/posts/{id}'
-            
-            if rating in {'q', 'e'}: # rating:questionable or rating:explicit
+
+            # Ignore ecchi images
+            if post['rating'] in {'q', 'e'}:  # rating:questionable or rating:explicit
                 logging.info(f'The image is too ecchi !: {danbooru_url}')
+                continue
 
-                await ctx.respond(f'This post is too ecchi for this bot ! Try again to fetch another image')
-                return
-
+            # Send to discord
             await ctx.respond(f'Here we go ! *{tag}* <:521089271:1320383029876883557> \n<{danbooru_url}> \n{file_url}')
-
             logging.info(f'The image was sent !: {danbooru_url}')
+            sent = True
+            break
+
+        if not sent:
+            await ctx.respond(f'This post is too ecchi for this bot ! Try again to fetch another image')
 
 def setup(bot):
     bot.add_cog(DanbooruCommand(bot))
