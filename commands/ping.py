9bd7c5405b3c754ba2180c1b9197032e4b5ece25
verniy1606 <158278526+verniy1606@users.noreply.github.com>
2025-01-01 05:33:13 +0900

    ping

--- commands/ping.py
+++ commands/ping.py
@@ -7,10 +7,8 @@ class PingCommand(commands.Cog):
 
     @commands.slash_command(name="ping")
     async def ping(self, ctx: discord.ApplicationContext):
-        if ctx.author.bot:
-            await ctx.respond("omae dare")
         latency = round(self.bot.latency*1000, 2)
-        await ctx.respond(f"{latency}ms かかりました!")
+        await ctx.respond(f"It took {latency}ms !")
 
 def setup(bot):
     bot.add_cog(PingCommand(bot))
